30b2d75b8dd29b7ee2791db1687163871ddecb3c
EvertonWallace <everton542@hotmail.com>
2024-08-25 11:59:35 -0300

    criacao da funcao register

--- ts/submitForms.ts
+++ ts/submitForms.ts
@@ -9,3 +9,16 @@ function login(): void {
 
     alert(`Email: ${email} \n Senha: ${password}`);
 }
+
+function register(): void {
+    const nameElements = document.getElementsByName('nameRegister') as NodeListOf<HTMLInputElement>;
+    const emailElements = document.getElementsByName('emailRegister') as NodeListOf<HTMLInputElement>;
+    const passwordElements = document.getElementsByName('passwordRegister') as NodeListOf<HTMLInputElement>;
+
+    // Obtendo os valores dos elementos
+    const name = nameElements[0].value;
+    const email = emailElements[0].value;
+    const password = passwordElements[0].value;
+
+    alert(`Nome: ${name} \n Email: ${email} \n Senha: ${password}`);
+}
