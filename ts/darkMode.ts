345c1822f28805c9acd95f684f4d4a609d1345b3
EvertonWallace <everton542@hotmail.com>
2024-08-25 11:54:29 -0300

    inicializacao das constantes que seram utilizadas na funcionalidade de mudanca de tema da pagina

---
+++ ts/darkMode.ts
@@ -0,0 +1,12 @@
+// Constantes que definem os ícones para os modos escuro e claro
+const DARK_MODE_ICON = 'la-moon';
+const LIGHT_MODE_ICON = 'la-sun';
+
+// Constantes que definem as variáveis de CSS para cores de fundo
+const DARK_MODE_BG_COLOR = '--dark-mode-bg-color';
+const DARK_MODE_COL_BG_COLOR = '--dark-mode-bg-col-color';
+const LIGHT_MODE_BG_COLOR = '--background';
+
+// Constantes para cores específicas em modo claro/escuro
+const WHITE_COLOR = '--white-color';
+const BLACK_COLOR = '--black-color';
